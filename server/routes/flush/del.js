91efb338100c9650a0ade24c098f88d42db0f65c
Kurdi Szabolcs <x-Szabolcs.Kurdi@wizzair.com>
2016-06-27 17:56:03 +0200

    fix(flush): project name parsing and validation

--- server/routes/flush/del.js
+++ server/routes/flush/del.js
@@ -1,9 +1,15 @@
+const joi = require('joi')
+const joiValidate = require('../../utils/joiValidate')
 const db = require('../../models/db')
+
+const paramsSchema = {
+  project: joi.string().lowercase().token().max(64)
+}
 
 // flush everything from collection
 module.exports = function * () {
-  const params = this.params
-  const collection = db.collection(params.project)
+  const params = joiValidate(this.params, paramsSchema)
+  const collection = db.collection(`p_${params.project}`)
   collection.remove({})
   this.body = null
 }
