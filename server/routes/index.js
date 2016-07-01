0a4c88270c5c6be992a55d04fbd0e1b03bd40af5
Kurdi Szabolcs <x-Szabolcs.Kurdi@wizzair.com>
2016-07-01 15:46:10 +0200

    refactor(project): delete collection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 const Router = require('koa-router')
 const proxy = require('./proxy')
 const rtConfig = require('./config')
-const flush = require('./flush')
 const projects = require('./projects')
 const router = new Router()
 
@@ -12,10 +11,10 @@ router
   .patch('/:project/proxy/*', proxy.patch)
   .delete('/:project/proxy/*', proxy.delete)
   .get('/api/projects', projects.get)
+  .delete('/api/projects/:project', projects.delete)
   .get('/api/config', rtConfig.get)
   .put('/api/config', rtConfig.put)
   .put('/api/config/toggle-project/:project', rtConfig.toggleProject.put)
-  .delete('/api/flush/:project', flush.delete)
 
 module.exports = {
   getRoutes: () => router.routes()
--- server/routes/projects/index.js
+++ server/routes/projects/index.js
@@ -1,5 +1,7 @@
 const get = require('./get')
+const del = require('./del')
 
 module.exports = {
-  get: get
+  get: get,
+  delete: del
 }
